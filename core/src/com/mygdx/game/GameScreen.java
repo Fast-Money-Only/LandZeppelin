47acf9ef82575858c7b73d88b73d087603e50900
m4gn3s1um <madmedmig@gmail.com>
2024-05-17 12:01:35 +0200

    Initial commit

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,91 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameScreen implements Screen {
+    final GameZep game;
+    Texture zepImage;
+    Texture background;
+    OrthographicCamera camera;
+    Zeppelin zeppelin;
+    float windX;
+    float windY;
+
+    public GameScreen(final GameZep game) {
+        this.game = game;
+
+        // Load images
+        zepImage = new Texture(Gdx.files.internal("zeppelin.png"));
+        background = new Texture(Gdx.files.internal("zeplandbackground.png"));
+
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
+
+        // Initialize the Zeppelin
+        zeppelin = new Zeppelin(400, 300, 128, 64, 60, 30, 30);
+
+        // Initialize wind with random direction and strength
+        //windX = MathUtils.random(-25, 25); // Random wind strength between -50 and 50
+        windX = MathUtils.random(-4,4);
+        windY = 0; // No vertical wind for simplicity
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        boolean moveLeft = Gdx.input.isKeyPressed(Keys.LEFT);
+        boolean moveRight = Gdx.input.isKeyPressed(Keys.RIGHT);
+        boolean moveUp = Gdx.input.isKeyPressed(Keys.UP);
+        boolean moveDown = Gdx.input.isKeyPressed(Keys.DOWN);
+
+        // Apply wind effect in the update
+        zeppelin.setWind(windX, windY);
+        zeppelin.update(delta, moveLeft, moveRight, moveUp, moveDown);
+
+        game.batch.begin();
+        game.batch.draw(background,0,0, camera.viewportWidth, camera.viewportHeight);
+        game.batch.draw(zepImage, zeppelin.getBounds().x, zeppelin.getBounds().y, zeppelin.getBounds().width, zeppelin.getBounds().height);
+        game.font.draw(game.batch, "Wind speed: " + windX, 0, 480);
+        game.batch.end();
+
+        if(zeppelin.getBounds().y < 10 && zeppelin.getBounds().x > 300){
+            zeppelin.getBounds().y = 400;
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        zepImage.dispose();
+    }
+}
+
---
+++ core/src/com/mygdx/game/Zeppelin.java
@@ -0,0 +1,76 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class Zeppelin {
+    private Rectangle bounds;
+    private float maxSpeed;
+    private float acceleration;
+    private float deceleration;
+    private float velocityX;
+    private float velocityY;
+    private float windX;
+    private float windY;
+
+    // Adjust this factor to increase wind effect
+    private final float windEffectMultiplier = 5.0f;
+
+    public Zeppelin(float x, float y, float width, float height, float maxSpeed, float acceleration, float deceleration) {
+        bounds = new Rectangle(x, y, width, height);
+        this.maxSpeed = maxSpeed;
+        this.acceleration = acceleration;
+        this.deceleration = deceleration;
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.windX = 0;
+        this.windY = 0;
+    }
+
+    public void setWind(float windX, float windY) {
+        this.windX = windX;
+        this.windY = windY;
+    }
+
+    public void update(float delta, boolean moveLeft, boolean moveRight, boolean moveUp, boolean moveDown) {
+        if (moveLeft) {
+            velocityX -= acceleration * delta;
+        } else if (moveRight) {
+            velocityX += acceleration * delta;
+        } else {
+            if (velocityX > 0) {
+                velocityX -= deceleration * delta;
+            } else if (velocityX < 0) {
+                velocityX += deceleration * delta;
+            }
+        }
+
+        if (moveUp) {
+            velocityY += acceleration * delta;
+        } else if (moveDown) {
+            velocityY -= acceleration * delta;
+        } else {
+            if (velocityY > 0) {
+                velocityY -= deceleration * delta;
+            } else if (velocityY < 0) {
+                velocityY += deceleration * delta;
+            }
+        }
+
+        // Apply wind effect
+        velocityX += windX * windEffectMultiplier * delta;
+        velocityY += windY * windEffectMultiplier * delta;
+
+        // Clamp velocities to max speed
+        if (velocityX > maxSpeed) velocityX = maxSpeed;
+        if (velocityX < -maxSpeed) velocityX = -maxSpeed;
+        if (velocityY > maxSpeed) velocityY = maxSpeed;
+        if (velocityY < -maxSpeed) velocityY = -maxSpeed;
+
+        bounds.x += velocityX * delta;
+        bounds.y += velocityY * delta;
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+}
